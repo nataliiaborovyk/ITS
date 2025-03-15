425aa0a121a7e6171c77a4de087572c63fcfc2ad
nataliiaborovyk <nataliia.borovyk@gmail.com>
2025-03-15 12:55:36 +0100

    esercizi classi

---
+++ Lezione_06/esercizio_02_slide.py
@@ -0,0 +1,29 @@
+'''
+Exercise 2 (Folder 9 ex_2.py)
+1. Write a class called Student with the attributes name (str) and studyProgram (str)
+2. Create three instances. One for yourself, one for your left neighbour and one
+for our right neighbour.
+3. Add a method printInfo that prints the name and studyProgram of a Student. 
+Test your method on the objects!
+4. Modify the code and add age and gender to the attributes. Modify your
+printing methods respectively too.
+'''
+print("\n   Esercizio 2 da slide")
+
+class Student:
+    def __init__(self, name:str, studyProgram:str, age:int, gender:str):
+        self.name = name
+        self.studyProgram = studyProgram
+        self.age = age
+        self.gender = gender
+
+    def print_info(self):
+        print(f"{self.name} - study program: {self.studyProgram}, age: {self.age}, gender: {self.gender}") 
+
+nat = Student("Nataliia", "Data Analyst", 40, "femmina")
+lor = Student("Lorenzo","Claud developer", 27, "maschio")        
+ele = Student("Eleonora", "Data analyst", 46, "femmina")
+
+nat.print_info()
+lor.print_info()
+ele.print_info()
