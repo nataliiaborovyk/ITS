c17d6018adc866e3c90493f264a1ac9dc7041f47
nataliiaborovyk <nataliia.borovyk@gmail.com>
2025-03-18 01:13:06 +0100

    pynative

--- Altro/PYnative/pynative_functions/ex_p01.py
+++ Altro/PYnative/pynative_functions/ex_p01.py
@@ -4,28 +4,31 @@ Write a program to create a function that takes two arguments,
 name and age, and print their value.
 '''
 # versione 1
+print("\nversione 1")
 
 def date(name: str, age: int) -> dict[str, int]:
     return {"Name":name, "age": age}
 
-user= date("Nat", 40)
-#print(f"Name: {user["Name"]}, age: {user["age"]}\n")
-print(user["Name"])
-print(user["age"])
-
+user= date("Tom", 40)
+print("Name: ",user["Name"], "Age: ",user["age"])
 
 
 # versione 2
+print("\nversione 2")
 
 def date_2(name:str, age:int):
-    print(f"\nName is: {name} and age is: {age}")
+    print(f"Name: {name} and age: {age}")
 
-date_2("Nataliia", 25)
+date_2("Alice", 25)
 
+
+# versione 3
+print("\nversione 3")
 
 def date_3(**kwargs) -> dict[str, int]:
     for name, age in kwargs.items():
-        print(f"\n{name}: {age}")
+        print(f"{name}: {age}", end=" ")
+    print("")
     return {"Name": name, "Age": age}
   
-print(date_3(name="Nat", age=40))
+print(date_3(name="Tom", age=40))
